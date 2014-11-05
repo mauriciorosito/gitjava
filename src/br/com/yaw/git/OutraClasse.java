dc8bf713078c534e2ccbbfc93ee9dcca67963f68
mauriciorosito <mauricio.rosito@bento.ifrs.edu.br>
2014-11-05 15:52:10 -0200

    Adicionando OutraClasse

---
+++ src/br/com/yaw/git/OutraClasse.java
@@ -0,0 +1,15 @@
+package br.com.yaw.git;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Professor
+ */
+public class OutraClasse {
+    
+}
